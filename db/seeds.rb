356caca60d91bd3d64950cb23d3d01ce6753d17f
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-06-06 13:44:41 -0400

    minor update to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: "Jane Doe")
 # User.create(username: "Reid Hoffman")
