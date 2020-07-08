7c8e394ea6893599d93cc1dd5ee899ce63d326f0
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-08 17:22:24 -0400

    begins user auth framework

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :contacts
     has_many :jobs 
 
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :username, length: { minimum: 6 }
+
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 end
